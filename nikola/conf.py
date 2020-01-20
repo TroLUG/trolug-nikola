6a254436473e6ce88c11552a2b7ddf38a8dbcd07
Jonas Stein <news@jonasstein.de>
2020-01-20 19:15:48 +0100

    conf: theme=bootblog4

--- nikola/conf.py
+++ nikola/conf.py
@@ -91,7 +91,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+THEME = "bootblog4"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
